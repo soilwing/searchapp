335be3e6226e829e5a9d33453532411dc431780e
soilwing <72150399+soilwing@users.noreply.github.com>
2023-04-18 01:38:42 +0900

    subscribers_count

--- lib/api_search.dart
+++ lib/api_search.dart
@@ -1,9 +1,9 @@
 import 'dart:convert';
-import 'dart:developer';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart' as http;
 
+//検索ワード、結果の管理
 class SearchNotifier extends StateNotifier<List<SearchResults>> {
   SearchNotifier() : super([]);
 
@@ -12,12 +12,12 @@ class SearchNotifier extends StateNotifier<List<SearchResults>> {
     state = results;
   }
 }
-
 final searchProvider =
-StateNotifierProvider<SearchNotifier, List<SearchResults>>(
-      (ref) => SearchNotifier(),
+    StateNotifierProvider<SearchNotifier, List<SearchResults>>(
+  (ref) => SearchNotifier(),
 );
 
+//GitHub APIを使用してリポジトリを検索
 Future<List<SearchResults>> githubSearch(String searchText) async {
   const sort = 'stars';
   final url =
@@ -31,8 +31,6 @@ Future<List<SearchResults>> githubSearch(String searchText) async {
       final results = itemList
           .map((item) => SearchResults.fromJson(item as Map<String, dynamic>))
           .toList();
-      //log(results.toString());
-      inspect(results);
       return results;
     } else {
       return [];
@@ -41,17 +39,35 @@ Future<List<SearchResults>> githubSearch(String searchText) async {
     return [];
   }
 }
+// リポジトリ詳細からsubscribers_countの値を取得しWatcher数とする
+// (watchers_countとstargazers_countに同じ値が設定されているため)
+Future<int> getSubscribers(String fullName) async {
+  final url = 'https://api.github.com/repos/$fullName';
 
+  try {
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      return data['subscribers_count'];
+    } else {
+      return 0;
+    }
+  } catch (e) {
+    return 0;
+  }
+}
+
+//jsonから必要な値を抜き出し整形
 class SearchResults {
   SearchResults(
-      this.name,
-      this.avatarUrl,
-      this.language,
-      this.stargazersCount,
-      this.watchersCount,
-      this.forksCount,
-      this.openIssuesCount,
-      );
+    this.name,
+    this.avatarUrl,
+    this.language,
+    this.stargazersCount,
+    this.watchersCount,
+    this.forksCount,
+    this.openIssuesCount,
+  );
 
   String name;
   String avatarUrl;
@@ -61,14 +77,21 @@ class SearchResults {
   int forksCount;
   int openIssuesCount;
 
-  factory SearchResults.fromJson(Map<String, dynamic> json) => SearchResults(
-    json['name'] as String,
-    json['owner']['avatar_url'] as String,
-    json['language'] as String?,
-    json['stargazers_count'] as int,
-    json['watchers_count'] as int,
-    json['forks_count'] as int,
-    json['open_issues_count'] as int,
-  );
+  factory SearchResults.fromJson(Map<String, dynamic> json) {
+    final searchResults = SearchResults(
+      json['full_name'] as String,
+      json['owner']['avatar_url'] as String,
+      json['language'] as String?,
+      json['stargazers_count'] as int,
+      0,
+      json['forks_count'] as int,
+      json['open_issues_count'] as int,
+    );
+    searchResults.loadWatchersCount();
+    return searchResults;
+  }
 
+  Future<void> loadWatchersCount() async {
+    watchersCount = await getSubscribers(name);
+  }
 }
